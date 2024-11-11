25f87b0a060e89a262a108be0d8a4fca92fc7007
Muhammad Haviv <75481735+muh-apip@users.noreply.github.com>
2024-11-11 20:51:47 +0700

    LandingPage

---
+++ EcoWheater-Planner/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './components/Header/Header';
+import HeroSection from './components/HeroSection/HeroSection';
+import Footer from './components/Footer/Footer';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <HeroSection />
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
